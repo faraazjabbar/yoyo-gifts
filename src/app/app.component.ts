9be55c5a84575955968fa74d6bd8072e4fabb828
Samir Kumar <Samir.Kumar@hgv.com>
2019-05-20 10:43:49 +0530

    email js test and ngrx store modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as emailjs from 'emailjs-com';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'yoyo-gifts';
+export class AppComponent implements OnInit {
+    title = 'yoyo-gifts';
+
+    ngOnInit() {
+        console.log('Start: emailjs-com');
+
+        const templateParams = {
+            email: 'Vishnu.Paturu@mindtree.com',
+            name: 'Samir',
+            notes: 'Check this out!'
+        };
+        const emailJsServiceId = 'gmail';
+        const emailJsTemplateId = 'template_Cg1kIF0Z';
+        const emailJsUserId = 'user_1Vb8OYU8eOTkZpWt24PNf';
+
+        // emailjs.send(emailJsServiceId, emailJsTemplateId, templateParams, emailJsUserId)
+        //     .then((response) => {
+        //         console.log('SUCCESS!', response.status, response.text);
+        //     }, (err) => {
+        //         console.log('FAILED...', err);
+        //     });
+
+        console.log('End: emailjs-com');
+    }
+
 }
